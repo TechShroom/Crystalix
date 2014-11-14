cd398656b6de3be2e94f80c5643969159abaf18c
Kenzie Togami <ket1999@gmail.com>
2014-11-14 09:58:07 -0800

    source jars and mod file
    [ci skip] cause tscore

---
+++ src/com/techshroom/mods/crystalix/CrystalixMod.java
@@ -0,0 +1,13 @@
+package com.techshroom.mods.crystalix;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = CrystalixMod.ID, name = CrystalixMod.USER_FRIENDLY_NAME,
+        acceptedMinecraftVersions = CrystalixMod.ACCEPTED_VERSIONS,
+        version = CrystalixMod.VERSION)
+public class CrystalixMod {
+    public static final String ID = "@MODID@";
+    public static final String VERSION = "@VERSION@";
+    public static final String ACCEPTED_VERSIONS = "@SUPPORTED_VERSIONS@";
+    public static final String USER_FRIENDLY_NAME = "Crystalix";
+}
