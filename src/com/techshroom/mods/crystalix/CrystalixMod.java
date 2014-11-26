71ec5f82fa36c9b58c1eb6ecc3697d24f84c7bda
Kenzie Togami <ket1999@gmail.com>
2014-11-26 08:17:13 -0800

    first block, barely works

--- src/com/techshroom/mods/crystalix/CrystalixMod.java
+++ src/com/techshroom/mods/crystalix/CrystalixMod.java
@@ -1,10 +1,23 @@
 package com.techshroom.mods.crystalix;
 
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
 import com.techshroom.mods.common.ClientProxy;
 import com.techshroom.mods.common.Proxy;
+import com.techshroom.mods.common.proxybuilders.HarvestData;
+import com.techshroom.mods.common.proxybuilders.PhasePrinter;
+import com.techshroom.mods.common.proxybuilders.RBBuilder;
+import com.techshroom.mods.common.proxybuilders.ToolLevel;
+import com.techshroom.mods.common.proxybuilders.ToolType;
+import com.techshroom.mods.crystalix.blocks.CrystalixOre;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLConstructionEvent;
 
 /**
  * Crystalix's main mod class.
@@ -35,6 +48,18 @@ public class CrystalixMod {
     @SidedProxy(clientSide = ClientProxy.QUALNAME, serverSide = Proxy.QUALNAME)
     private static Proxy PROXY;
 
+    @Instance
+    private static CrystalixMod MOD;
+
+    /**
+     * Mod instance.
+     * 
+     * @return the mod instance.
+     */
+    public static CrystalixMod mod() {
+        return MOD;
+    }
+
     /**
      * Get the proxy currently in use.
      * 
@@ -44,13 +69,36 @@ public class CrystalixMod {
         return PROXY;
     }
 
-    {
+    /**
+     * Creative tab: blocks
+     */
+    public final CreativeTabs tabBlocks = new CreativeTabs("crystalixBlocks") {
+        @Override
+        public Item getTabIconItem() {
+            return Item.getItemFromBlock(Block.getBlockFromName(ID
+                    + ":oreCrystalix"));
+        }
+    };
+
+    @SuppressWarnings("javadoc")
+    @EventHandler
+    public void construct(FMLConstructionEvent construct) {
+        PhasePrinter.addPrinter(proxy(), ID);
         proxySetup();
     }
 
-    /**
-     * Initialization code to register everything to the proxy
-     */
     private void proxySetup() {
+        Proxy p = proxy();
+        String oreBlockID = "oreCrystalix";
+        RBBuilder.NoTile<CrystalixOre> oreBlock =
+                new RBBuilder.NoTile<CrystalixOre>(CrystalixOre.class, oreBlockID);
+        oreBlock.setBlockName("oreCrystalix");
+        oreBlock.setBlockTextureName(ID + ":crystalix_ore");
+        oreBlock.setCreativeTab(tabBlocks);
+        oreBlock.setHarvestData(HarvestData.create(ToolType.Pickaxe.TYPE,
+                                                   ToolLevel.Iron.TYPE)
+                .asBlockHarvestData());
+        oreBlock.setSoundType(Block.soundTypeStone);
+        p.registerRegisterableObject(oreBlock);
     }
 }
---
+++ src/com/techshroom/mods/crystalix/blocks/CrystalixOre.java
@@ -0,0 +1,17 @@
+package com.techshroom.mods.crystalix.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * Ore of Crystalix.
+ * 
+ * @author Kenzie Togami
+ */
+public class CrystalixOre
+        extends Block {
+    @SuppressWarnings("javadoc")
+    public CrystalixOre() {
+        super(Material.rock);
+    }
+}
